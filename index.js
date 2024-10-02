b4d0c6a95e065f7a7dee2ff44c304d43e1176bce
Siva23078 <siva.k23078@gmail.com>
2024-10-02 16:57:47 +0530

    route structure

--- index.js
+++ index.js
@@ -9,10 +9,34 @@ app.get('/',(req , res)=>{
     res.json({msg: "hello student"});
 })
 
+// crud functianaly movies
+
+//r -for reading
+app.get("/movies", ()=>{
+
+})
+// c -for creating movies
+app.post("/movie",()=>{
+
+}) 
+
+// u - for updating movies
+app.put("/movie:id",()=>{
+
+})
+
+// d - for delete movies
+
+app.delete(".movies:id",()=>{
+
+})
+
 app.listen(port,()=>{
     console.log(`this server running at http://localhost${port} `);
 });
 
+
+// 
 
 
 
